9440c2f1edfb74af2e7efa5a66cc6bc07309433f
SebasSoledad07 <sebastiansg@ufps.edu.co>
2025-04-10 07:37:53 -0500

    proyecto creado

---
+++ src/main/java/co/edu/ufps/proyectoweb/ProyectoWebApplication.java
@@ -0,0 +1,13 @@
+package co.edu.ufps.proyectoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoWebApplication.class, args);
+    }
+
+}
